8a4e5c077d429c893921dcd4019d105c4646b629
Agah Ebrahimi <Agah@Agahs-MBP.fios-router.home>
2019-05-02 02:24:41 -0400

    correction

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,22 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      
+      buttonValue: "Hey"
     };
+
+    this.call = this.call.bind(this);
   }
+
+  call(){
+    fetch("https://sybsys.appspot.com/")
+    .then(results => results.text())
+    .then(data => this.setState({buttonValue: data}));
+  }
+
   render(){
     return (
       <div>
-         <Button>Click Here</Button>
+         <Button onClick={this.call}>{this.state.buttonValue}</Button>
       </div>
     );
   }
