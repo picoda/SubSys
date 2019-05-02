ae7801ee1d39c4c0ea266343f3d9566bdc7d778c
Agah Ebrahimi <Agah@Agahs-MBP.fios-router.home>
2019-05-02 02:21:27 -0400

    correction

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Button } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div>
-       <Button>Click Here</Button>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      
+    };
+  }
+  render(){
+    return (
+      <div>
+         <Button>Click Here</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
