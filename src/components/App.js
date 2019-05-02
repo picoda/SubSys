b0c774a34f7e3f2d1c58a7bacb76a7f3afb254f5
Agah Ebrahimi <Agah@Agahs-MBP.fios-router.home>
2019-05-01 23:59:32 -0400

    Files added

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div>
+       <Button>Click Here</Button>
+    </div>
+  );
+}
+
+export default App;
